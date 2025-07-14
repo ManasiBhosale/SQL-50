1909ac2d73757749fcf75aa5208fd85871e36bff
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:37:14 +0100

    Update and rename 24. User Activity for the Past 30 Days I.txt to 24. User Activity for the Past 30 Days I.sql

--- 24. User Activity for the Past 30 Days I.sql
+++ 24. User Activity for the Past 30 Days I.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/user-activity-for-the-past-30-days-i/description/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/user-activity-for-the-past-30-days-i/description/?envType=study-plan-v2&envId=top-sql-50
 
 
 # Write your MySQL query statement below
