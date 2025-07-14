1c87b1382db7213676898cffbdf3891e8b560733
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:32:35 +0100

    Update and rename 06. Replace Employee ID With The Unique Identifier.txt to 06. Replace Employee ID With The Unique Identifier.sql

--- 06. Replace Employee ID With The Unique Identifier.sql
+++ 06. Replace Employee ID With The Unique Identifier.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/description/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/description/?envType=study-plan-v2&envId=top-sql-50
 
 
 # Write your MySQL query statement below
