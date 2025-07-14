2c84d1b33a59148c819d99237966bd69895c4229
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:25:16 +0100

    Update 01. Recyclable and Low Fat Products.sql

--- 01. Recyclable and Low Fat Products.sql
+++ 01. Recyclable and Low Fat Products.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
 
 # Write your MySQL query statement below
 
