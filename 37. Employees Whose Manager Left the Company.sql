cc98f37bf96bf3134c76eda587e05c206a3a3141
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:40:19 +0100

    Update and rename 37. Employees Whose Manager Left the Company.txt to 37. Employees Whose Manager Left the Company.sql

--- 37. Employees Whose Manager Left the Company.sql
+++ 37. Employees Whose Manager Left the Company.sql
@@ -1,5 +1,4 @@
-https://leetcode.com/problems/employees-whose-manager-left-the-company/?envType=study-plan-v2&envId=top-sql-50
-
+# https://leetcode.com/problems/employees-whose-manager-left-the-company/?envType=study-plan-v2&envId=top-sql-50
 
 
 # Write your MySQL query statement below
