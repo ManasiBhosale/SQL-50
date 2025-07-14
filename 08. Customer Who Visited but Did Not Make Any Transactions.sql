598b91b379ace29d002b3ebb21d40f37ea967bd3
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:33:07 +0100

    Update and rename 08. Customer Who Visited but Did Not Make Any Transactions.txt to 08. Customer Who Visited but Did Not Make Any Transactions.sql

--- 08. Customer Who Visited but Did Not Make Any Transactions.sql
+++ 08. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/?envType=study-plan-v2&envId=top-sql-50
 
 
 # Write your MySQL query statement below
