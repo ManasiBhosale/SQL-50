6da8843b894122020c9d7cb3310e3077cb5defb5
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:33:23 +0100

    Update and rename 09. Rising Temperature.txt to 09. Rising Temperature.sql

--- 09. Rising Temperature.sql
+++ 09. Rising Temperature.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/rising-temperature/description/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/rising-temperature/description/?envType=study-plan-v2&envId=top-sql-50
 
 
 # Write your MySQL query statement below
