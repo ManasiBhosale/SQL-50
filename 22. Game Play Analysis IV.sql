a623e129932ce53ff40b61a84d89497d12ade83a
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:36:28 +0100

    Update and rename 22. Game Play Analysis IV.txt to 22. Game Play Analysis IV.sql

--- 22. Game Play Analysis IV.sql
+++ 22. Game Play Analysis IV.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/game-play-analysis-iv/description/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/game-play-analysis-iv/description/?envType=study-plan-v2&envId=top-sql-50
 
 # Write your MySQL query statement below
 
