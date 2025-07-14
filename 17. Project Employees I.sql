30d9151a64ca1f33646c151afd28e72a34461344
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:35:19 +0100

    Update and rename 17. Project Employees I.txt to 17. Project Employees I.sql

--- 17. Project Employees I.sql
+++ 17. Project Employees I.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/project-employees-i/description/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/project-employees-i/description/?envType=study-plan-v2&envId=top-sql-50
 
 
 # Write your MySQL query statement below
