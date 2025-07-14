4e42d6dc49eece9aaa67b1d5953b264451873c13
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:34:07 +0100

    Update and rename 12. Students and Examinations.txt to 12. Students and Examinations.sql

--- 12. Students and Examinations.sql
+++ 12. Students and Examinations.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/students-and-examinations/description/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/students-and-examinations/description/?envType=study-plan-v2&envId=top-sql-50
 
 
 
