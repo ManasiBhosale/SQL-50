ff89906940e1d4d1feeefd3a733ec05aacb8f756
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:39:45 +0100

    Update and rename 35. Last Person to Fit in the Bus.txt to 35. Last Person to Fit in the Bus.sql

--- 35. Last Person to Fit in the Bus.sql
+++ 35. Last Person to Fit in the Bus.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/last-person-to-fit-in-the-bus/description/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/last-person-to-fit-in-the-bus/description/?envType=study-plan-v2&envId=top-sql-50
 
 
 # Write your MySQL query statement below
