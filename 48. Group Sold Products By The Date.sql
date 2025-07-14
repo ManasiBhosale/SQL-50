01708af6534b258a79fe7a10da0d76a03e65570b
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:42:16 +0100

    Update and rename 48. Group Sold Products By The Date.txt to 48. Group Sold Products By The Date.sql

--- 48. Group Sold Products By The Date.sql
+++ 48. Group Sold Products By The Date.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/group-sold-products-by-the-date/description/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/group-sold-products-by-the-date/description/?envType=study-plan-v2&envId=top-sql-50
 
 # Write your MySQL query statement below
 
