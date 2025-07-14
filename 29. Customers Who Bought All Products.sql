5b035701c640982cb201334bfe2f96c785d290b3
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:38:19 +0100

    Update and rename 29. Customers Who Bought All Products.txt to 29. Customers Who Bought All Products.sql

--- 29. Customers Who Bought All Products.sql
+++ 29. Customers Who Bought All Products.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/customers-who-bought-all-products/description/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/customers-who-bought-all-products/description/?envType=study-plan-v2&envId=top-sql-50
 
 
 # Write your MySQL query statement below
