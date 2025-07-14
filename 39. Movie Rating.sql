f1b3b838554ecbdca0b68bd79c2f507816b5cea3
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:40:41 +0100

    Update and rename 39. Movie Rating.txt to 39. Movie Rating.sql

--- 39. Movie Rating.sql
+++ 39. Movie Rating.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/movie-rating/description/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/movie-rating/description/?envType=study-plan-v2&envId=top-sql-50
 
 
 -- # Write your MySQL query statement below
