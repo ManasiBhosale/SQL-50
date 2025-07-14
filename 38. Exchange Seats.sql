7e82169053a628b462c75762cc504231a583d41b
Manasi Bhosale <69459173+ManasiBhosale@users.noreply.github.com>
2025-07-14 16:40:30 +0100

    Update and rename 38. Exchange Seats.txt to 38. Exchange Seats.sql

--- 38. Exchange Seats.sql
+++ 38. Exchange Seats.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/exchange-seats/description/?envType=study-plan-v2&envId=top-sql-50
+# https://leetcode.com/problems/exchange-seats/description/?envType=study-plan-v2&envId=top-sql-50
 
 
 # Write your MySQL query statement below
